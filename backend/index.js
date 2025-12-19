4fb9398aef65039b0cc849b784a9f51704742659
Keyush-15 <Keyushkothari2001@gmail.com>
2025-12-19 16:13:39 +0530

    backend changes

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,8 @@ mongoose
 // Routes
 app.use("/api/tasks", taskRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-  });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
